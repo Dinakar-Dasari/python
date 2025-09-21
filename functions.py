27bb8f77669bd36c874f5a30ccab03708a61a9fb
Dustboy743 <dsaidns@gmail.com>
2025-09-22 00:54:30 +0530

    python

---
+++ functions.py
@@ -0,0 +1,8 @@
+def addition(num1, num2):
+    sum = num1+num2
+    return sum
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+sum = addition(a,b)
+print(f"Sum is {sum}")
